48e7ba4c457ac27af33cbc5b0c5ea63fdecc78fd
deaaalma <hopegekdea@gmail.com>
2026-06-27 23:12:32 +0800

    chore: clean up stray files and hardhat config

--- smart_contracts/hardhat.config.ts
+++ smart_contracts/hardhat.config.ts
@@ -15,6 +15,9 @@ const config: HardhatUserConfig = {
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 80002
     }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY || "YOUR_POLYGONSCAN_API_KEY"
   }
 };
 
